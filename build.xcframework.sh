bc3d74a9d43f03f30f7cbe5f868e455cea9b6c1b
akerele abraham <abrahamakerele38@gmail.com>
2025-06-04 10:13:02 +0100

    make buildframwork script a bit more robust

--- build.xcframework.sh
+++ build.xcframework.sh
@@ -5,21 +5,43 @@ rm -rf iosbuild/*
 mkdir -p iosbuild/stitcher
 mkdir -p iosbuild/libturbojpeg
 
-SIM_ARM_STITCHER_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios-sim/arm64/lib/libNativeStitcher.a
-SIM_X86_STITCHER_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios-sim/x86_64/lib/libNativeStitcher.a
-ARM_STITCHER_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios/arm64/lib/libNativeStitcher.a
+STITCHER_BUILD_PATH=/Users/haibeey/p-h/lendo-stuff/stitcher/installs
 
-SIM_ARM_TURBOJPEG_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios-sim/arm64/lib/libturbojpeg.a
-SIM_X86_TURBOJPEG_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios-sim/x86_64/lib/libturbojpeg.a
-ARM_TURBOJPEG_LIB=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios/arm64/lib/libturbojpeg.a
+SIM_ARM_STITCHER_LIB=$STITCHER_BUILD_PATH/native-stitcher/ios-sim/arm64/lib/libNativeStitcher.a
+SIM_X86_STITCHER_LIB=$STITCHER_BUILD_PATH/native-stitcher/ios-sim/x86_64/lib/libNativeStitcher.a
+ARM_STITCHER_LIB=$STITCHER_BUILD_PATH/native-stitcher/ios/arm64/lib/libNativeStitcher.a
 
-SIM_ARM_STITCHER_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios-sim/arm64/include/
-SIM_X86_STITCHER_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios-sim/x86_64/include/
-ARM_STITCHER_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/native-stitcher/ios/arm64/include/
+SIM_ARM_TURBOJPEG_LIB=$STITCHER_BUILD_PATH/libturbojpeg/ios-sim/arm64/lib/libturbojpeg.a
+SIM_X86_TURBOJPEG_LIB=$STITCHER_BUILD_PATH/libturbojpeg/ios-sim/x86_64/lib/libturbojpeg.a
+ARM_TURBOJPEG_LIB=$STITCHER_BUILD_PATH/libturbojpeg/ios/arm64/lib/libturbojpeg.a
 
-SIM_ARM_TURBOJPEG_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios-sim/arm64/include/
-SIM_X86_TURBOJPEG_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios-sim/x86_64/include/
-ARM_TURBOJPEG_HEADER=/Users/abrahamakerele/p-h/lendostuff/stitcher/installs/libturbojpeg/ios/arm64/include/
+SIM_ARM_STITCHER_HEADER=$STITCHER_BUILD_PATH/native-stitcher/ios-sim/arm64/include/
+SIM_X86_STITCHER_HEADER=$STITCHER_BUILD_PATH/native-stitcher/ios-sim/x86_64/include/
+ARM_STITCHER_HEADER=$STITCHER_BUILD_PATH/native-stitcher/ios/arm64/include/
+
+dirs=("SIM_ARM_STITCHER_HEADER" "SIM_X86_STITCHER_HEADER" "ARM_STITCHER_HEADER")
+files=("jpeglib.h" "jconfig.h" "turbojpeg.h" "jerror.h" "jmorecfg.h")
+
+for dir_var in "${dirs[@]}"; do
+    dir_path="${!dir_var}"
+    if [[ -d "$dir_path" ]]; then
+        for file in "${files[@]}"; do
+            target="$dir_path/$file"
+            if [[ -f "$target" ]]; then
+                echo "Deleting $target"
+                rm "$target"
+            else
+                echo "Not found: $target"
+            fi
+        done
+    else
+        echo "Directory not found or not set: $dir_var"
+    fi
+done
+
+SIM_ARM_TURBOJPEG_HEADER=$STITCHER_BUILD_PATH/libturbojpeg/ios-sim/arm64/include/
+SIM_X86_TURBOJPEG_HEADER=$STITCHER_BUILD_PATH/libturbojpeg/ios-sim/x86_64/include/
+ARM_TURBOJPEG_HEADER=$STITCHER_BUILD_PATH/libturbojpeg/ios/arm64/include/
 
 
 
